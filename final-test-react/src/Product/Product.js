00084b16461783a68d23444ae4732f6bd2c9a437
sofia <sofiaprado1@gmail.com>
2018-07-23 20:21:28 -0300

    single product

--- final-test-react/src/Product/Product.js
+++ final-test-react/src/Product/Product.js
@@ -1,64 +1,56 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Route, Link } from "react-router-dom";
 import SearchBox from '../SearchBox/SearchBox'
 import './Product.css';
 
 
 class Product extends Component {
-  constructor(props) { 
+  constructor(props) {
     super(props)
     this.state = {
       finalProducts: false,
       searchTitle: '',
       item: '',
       itemId: this.props.match.params.single,
-      showProduct: []
+      producto: ''
     }
   }
 
-//BRING PRODUCT DATA
-componentDidMount() {
-  const id = this.props.match.params.id
-  fetch('http://localhost:3001/api/items/'+ id).then((data) => {
-  return data.json()
-  }).then((result) => {
-    const item = result.item
-    console.log(item)
+  //BRING PRODUCT DATA
+  componentDidMount() {
+    const id = this.props.match.params.id
+    fetch('http://localhost:3001/api/items/' + id).then((data) => {
+      return data.json()
+    }).then((result) => {
+     console.log( 'RESULT ES', result)
+  
 
-  const array = [
-    result.description.plain_text,
-    item.currency,
-    item.title,
-    item.price,
-    item.sold,
-    item.image,
-    item.quantity,
-    item.condition
-    ]
-  this.setState({
-    showProduct: array
-  })
- })
-}
+      this.setState({
+        producto: result
+      })
+    })
+  }
 
-render() {
-  const showProduct = this.state.showProduct
-  return(
-    <div id="main-container">
-      <div>
-        <SearchBox />  
+  render() {
+    return (
+      <div id="main-container">
+        {this.state.producto.item && 
+        <div>
+          <img className='image' src={this.state.producto.item.image} />
+          <p className='condition'>{this.state.producto.item.condition} - {this.state.producto.quantity} vendidos</p>
+          <p className='title-txt'>{this.state.producto.item.title}</p>
+          <p className='price-txt'>$ {this.state.producto.item.price}</p>
+          <button>Comprar</button>
+          <p className='description'>{this.state.producto.description.plain_text}</p>   
+        </div>   
+        }
       </div>
-      <img className='image' src={showProduct[5]} />
-      <div id="item-data">
-      <p className='condition'>{showProduct[7]} - {showProduct[4]} vendidos</p>
-      <p className='title-txt'>{showProduct[2]}</p>
-      <p className='price-txt'>$ {showProduct[3]}</p>
-      <button>Comprar</button>
-      </div>
-      <p className='description'>{showProduct[0]}</p>  
-    </div>
-  );
-}
+   )    
+  } 
 }
 
 export default Product;
+
+
+
+
